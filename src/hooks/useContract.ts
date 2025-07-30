8fb0bee82999a2067f0a2c3b70f8844ecfe3052b
1355600640 <1355600640@qq.com>
2025-07-31 00:17:54 +0800

    Add logging for USDT amount calculation in callContractMethod to enhance debugging and trace contract interactions.

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -302,6 +302,8 @@ export async function callContractMethod(amount: number,address?:string) {
       * @param amount 支付的 ERC20 代币总额。
       * @param shareAmount 在 ERC20 代币支付总额中，用于 Shares 分佣的代币量。
       */
+     console.log("usdt:",usdt,"amount:",(amount*0.02).toString(),ethers.parseUnits((amount*0.02).toString(), decimals));
+     
       const tx = await contract.payAndShareWithERC20(
         address || '0xa8d578052b23eeceae4cdf74de654b2a5a8f29a7', // 使用当前用户地址作为收款地址
         usdtAddress,

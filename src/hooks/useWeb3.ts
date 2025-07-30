3ddff24267f1734a32907f525d9027bffb3cde88
1355600640 <1355600640@qq.com>
2025-07-30 22:20:34 +0800

    Refactor useWeb3 hook to improve wallet connection logic; add useEffect for continuous wallet status checks and rename connectWallet function for clarity.

--- src/hooks/useWeb3.ts
+++ src/hooks/useWeb3.ts
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import { connectWallet as connectWalletContract } from './useContract';
 // import { CommissionParams, TransactionStatus } from '../types/contract';
@@ -12,8 +12,7 @@ export const useWeb3 = () => {
   const [error, setError] = useState<string>('');
   // const ethereum = (window as any).ethereum;
 
-  const connectWallet =async ()=>{
-    await connectWalletContract()
+  const wallectInfo = async() => {
     const signer = await (window as any).ethersProvider.getSigner();
     setSigner(signer);
     setAccount(await signer.getAddress());
@@ -22,20 +21,51 @@ export const useWeb3 = () => {
     setIsConnecting(false);
     setError('');
   }
+  if ((window as any).deboxWallet) {
+    wallectInfo()
+  }
+
+  const connectWallet = async () => {
+    await connectWalletContract()
+    wallectInfo()
+
+  }
 
   const formatAddress = (address: string) => {
     return address.slice(0, 6) + '...' + address.slice(-4);
   }
 
+  useEffect(()=>{
+    const interval = setInterval(async() => { 
+      if((window as any).deboxWallet){
+        const signer = await (window as any).ethersProvider.getSigner();
+        if(await signer.getAddress()){
+          wallectInfo()
+        }else {
+          setIsConnected(false)
+          setIsConnecting(false)
+          setError('')
+          setAccount('')
+          setSigner(null)
+          setProvider(null)
+        }
+      }
+    }, 1000);
+    return ()=>{
+      clearInterval(interval)
+    }
+  },[])
+
+
   // 连接钱包
- return {
-  connectWallet,
-  signer,
-  account,
-  isConnected,
-  isConnecting,
-  error,
-  formatAddress,
-  provider
- }
+  return {
+    connectWallet,
+    signer,
+    account,
+    isConnected,
+    isConnecting,
+    error,
+    formatAddress,
+    provider
+  }
 }; 
